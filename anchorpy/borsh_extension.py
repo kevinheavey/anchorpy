338cdc28fa9c809ecbc8f987c70e2d79bb7c8cb6
kevinheavey <kevinheavey123@gmail.com>
2021-09-27 11:07:29 +0100

    fix publickey borsh

--- anchorpy/borsh_extension.py
+++ anchorpy/borsh_extension.py
@@ -1,17 +1,17 @@
 from construct import Adapter
 from solana import publickey
-from borsh import String
+from construct import Bytes
 
 
 class _PublicKey(Adapter):
     def __init__(self) -> None:
-        super().__init__(String)  # type: ignore
+        super().__init__(Bytes(32))  # type: ignore
 
-    def _decode(self, obj: str, context, path) -> publickey.PublicKey:
+    def _decode(self, obj: bytes, context, path) -> publickey.PublicKey:
         return publickey.PublicKey(obj)
 
-    def _encode(self, obj: publickey.PublicKey, context, path) -> str:
-        return str(obj)
+    def _encode(self, obj: publickey.PublicKey, context, path) -> bytes:
+        return bytes(obj)
 
 
 PublicKey = _PublicKey()
