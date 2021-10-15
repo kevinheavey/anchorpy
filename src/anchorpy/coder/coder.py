b1e902bb2fa06200c93e870a97729c199fd693e9
kevinheavey <kevinheavey123@gmail.com>
2021-10-15 15:58:16 +0100

    docs

--- src/anchorpy/coder/coder.py
+++ src/anchorpy/coder/coder.py
@@ -5,7 +5,14 @@ from anchorpy.idl import Idl
 
 
 class Coder:
+    """Coder provides a facade for encoding and decoding all IDL related objects."""
+
     def __init__(self, idl: Idl):
+        """Initialize the coder.
+
+        Args:
+            idl: a parsed Idl instance.
+        """
         self.instruction: InstructionCoder = InstructionCoder(idl)
         self.accounts: AccountsCoder = AccountsCoder(idl)
         self.events: EventCoder = EventCoder(idl)
