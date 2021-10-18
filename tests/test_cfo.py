8ef26689bcf247e4037fd7ce20d350adc31944de
kevinheavey <kevinheavey123@gmail.com>
2021-10-18 10:11:21 +0100

    add test_csfo (incomplete)

---
+++ tests/test_cfo.py
@@ -0,0 +1,21 @@
+import subprocess
+from pathlib import Path
+
+from pytest import fixture
+
+from tests.utils import get_localnet
+
+PATH = Path("anchor/tests/cfo")
+
+localnet = get_localnet(PATH)
+
+
+@fixture(scope="module")
+def build_lockup() -> None:
+    subprocess.run(  # noqa: S603
+        ["anchor", "build"], check=True, cwd="anchor/tests/lockup"
+    )
+
+
+def boilerplate(build_lockup, localnet):
+    """TODO."""
