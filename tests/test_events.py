4d153f7747bf22c7695894f9512391b853351127
kevinheavey <kevinheavey123@gmail.com>
2021-10-29 13:09:07 +0100

    add websockets

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,13 +1,18 @@
-"""Mimics anchor/tests/errors/tests/events.js."""
+"""Mimics anchor/tests/errors/tests/events.js.
+
+Note: this is unfinished.
+"""
 import asyncio
+import json
 from pathlib import Path
 import websockets
-from pytest import raises, mark, fixture
-from anchorpy import ProgramError, Program, create_workspace, close_workspace, Context
-from solana.keypair import Keypair
-from solana.sysvar import SYSVAR_RENT_PUBKEY
-from solana.transaction import AccountMeta, Transaction, TransactionInstruction
-from solana.rpc.core import RPCException
+from pytest import mark, fixture
+from anchorpy import (
+    Program,
+    create_workspace,
+    close_workspace,
+    EventParser,
+)
 from tests.utils import get_localnet
 
 PATH = Path("anchor/tests/events/")
@@ -26,14 +31,25 @@ def event_loop():
 @fixture(scope="module")
 async def program(localnet) -> Program:
     workspace = create_workspace(PATH)
-    yield workspace["errors"]
+    yield workspace["events"]
     await close_workspace(workspace)
 
 
 @mark.asyncio
 async def test_initialize(program: Program) -> None:
-    uri = "http://127.0.0.1:8900"
+    uri = "ws://127.0.0.1:8900"
     async with websockets.connect(uri) as websocket:
+        await websocket.send(
+            '{"jsonrpc": "2.0", "id": 1, "method": "logsSubscribe", "params": ["all"]}'
+        )
+        received = await websocket.recv()
         await program.rpc["initialize"]()
-        for message in websocket:
-            print(message)
+        received = await websocket.recv()
+        as_json = json.loads(received)
+        logs = as_json["params"]["result"]["value"]["logs"]
+        parser = EventParser(program.program_id, program.coder)
+        parsed = []
+        parser.parse_logs(logs, lambda evt: parsed.append(evt))
+        event = parsed[0]
+        assert event["data"]["data"] == 5
+        assert event["data"]["label"] == "hello"
