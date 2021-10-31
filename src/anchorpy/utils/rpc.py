51b08c85fb337b7d529a83860e51d7485a14685f
kevinheavey <kevinheavey123@gmail.com>
2021-10-31 20:01:13 +0000

    add utils.rpc.invoke

---
+++ src/anchorpy/utils/rpc.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+from solana.transaction import (
+    AccountMeta,
+    Transaction,
+    TransactionInstruction,
+    TransactionSignature,
+)
+from anchorpy.program.common import AddressType, translate_address
+from anchorpy.provider import Provider
+
+
+async def invoke(
+    program_id: AddressType,
+    provider: Provider,
+    accounts: Optional[List[AccountMeta]] = None,
+    data: Optional[bytes] = None,
+) -> TransactionSignature:
+    """Sends a transaction to a program with the given accounts and instruction data."""
+    program_id = translate_address(program_id)
+    tx = Transaction()
+    tx.add(
+        TransactionInstruction(
+            program_id=program_id,
+            keys=[] if accounts is None else accounts,
+            data=bytes(0) if data is None else data,
+        ),
+    )
+    return await provider.send(tx)
