1f779fe739192ecc372b2ec2da2f51d17d1ee13a
kevinheavey <kevinheavey123@gmail.com>
2021-10-31 16:19:13 +0000

    update expected_raw

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -138,7 +138,7 @@ async def test_can_retrieve_events_when_simulating_transaction(
         "Program log: jvbowsvlmkcJAAAA",
         (
             "Program Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS consumed "
-            "4693 of 200000 compute units"
+            "4694 of 200000 compute units"
         ),
         "Program Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS success",
     ]
