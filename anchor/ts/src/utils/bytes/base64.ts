b17cb8eda8d57d8c3ea70243b50496052d663018
kevinheavey <kevinheavey123@gmail.com>
2021-11-13 14:04:53 +0000

    use submodules instead of subtree

--- anchor/ts/src/utils/bytes/base64.ts
+++
@@ -1,9 +0,0 @@
-import * as base64 from "base64-js";
-
-export function encode(data: Buffer): string {
-  return base64.fromByteArray(data);
-}
-
-export function decode(data: string): Buffer {
-  return Buffer.from(base64.toByteArray(data));
-}
--- anchor/ts/src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * as sha256 from "./sha256";
-export * as rpc from "./rpc";
-export * as publicKey from "./pubkey";
-export * as bytes from "./bytes";
-export * as token from "./token";
-export * as features from "./features";
