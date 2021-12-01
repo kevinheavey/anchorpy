8f4037d137f0668c9c2d6164cc49c26abb39ce2e
kevinheavey <kevinheavey123@gmail.com>
2021-12-01 23:14:38 +0000

    take version spec out of noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 from nox_poetry import session  # type: ignore
 
 
-@session(python=["3.9"])
+@session
 def tests(session):  # noqa: D103,WPS442
     session.run_always("poetry", "install", external=True)
     session.install(".")
