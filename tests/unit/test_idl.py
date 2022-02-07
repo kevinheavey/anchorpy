e5e13f3d50842e4029067d9591a72c8493bb643c
Kevin Heavey <kevinheavey123@gmail.com>
2022-02-07 16:12:26 +0000

    test that we don't crash when loading program with tuple variants

--- tests/unit/test_idl.py
+++ tests/unit/test_idl.py
@@ -1,11 +1,9 @@
 import json
 from pathlib import Path
-from pytest import mark
 from anchorpy import Idl, Program
 from solana.publickey import PublicKey
 
 
-@mark.unit
 def test_idls() -> None:
     idls = []
     programs = []
@@ -19,7 +17,6 @@ def test_idls() -> None:
     assert idls
 
 
-@mark.unit
 def test_jet_enum() -> None:
     path = Path("tests/idls/jet.json")
     with path.open() as f:
@@ -29,3 +26,11 @@ def test_jet_enum() -> None:
     expired_err = program.type["CacheInvalidError"].Expired
     assert expired_err(msg="hi").msg == "hi"
     assert expired_err._sumtype_attribs[0][1].type == str
+
+
+def test_switchboard_tuple() -> None:
+    path = Path("tests/idls/switchboard.json")
+    with path.open() as f:
+        data = json.load(f)
+    idl = Idl.from_json(data)
+    program = Program(idl, PublicKey(1))  # noqa: F841
