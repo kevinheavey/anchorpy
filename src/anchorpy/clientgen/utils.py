c0072c7ac795691f5f5ce6c55e305f3fda8a6b1b
Kevin Heavey <kevinheavey123@gmail.com>
2022-04-18 07:08:38 +0100

    add dataclass

--- src/anchorpy/clientgen/utils.py
+++ src/anchorpy/clientgen/utils.py
@@ -1,11 +1,11 @@
 from typing import List, Iterator
-from genpy import Function as FunctionOriginal, Generable, Suite
+from genpy import Function as FunctionOriginal, Generable, Suite, Class
 
 
 class TypedParam(Generable):
     def __init__(self, name: str, type_: str) -> None:
         self.name = name
-        self.type_ = type_
+        self.type = type_
 
     def generate(self) -> Iterator[str]:
         yield f"{self.name}: {self.type}"
@@ -20,6 +20,7 @@ class Break(Generable):
     def generate(self):
         yield "break"
 
+
 class Union(Generable):
     def __init__(self, members: list[str]) -> None:
         self.members = members
@@ -27,6 +28,8 @@ class Union(Generable):
     def generate(self) -> Iterator[str]:
         joined = ",".join(self.members)
         yield f"Union[{joined}]"
+
+
 class Function(FunctionOriginal):
     def __init__(
         self, name: str, args: List[TypedParam], body: Generable, return_type: str
@@ -35,11 +38,20 @@ class Function(FunctionOriginal):
         self.return_type = return_type
 
     def generate(self) -> Iterator[str]:
-        arg_strings = [f"{arg.name}: {arg.type_}" for arg in self.args]
+        arg_strings = [f"{arg.name}: {arg.type}" for arg in self.args]
         yield "def {}({}): -> {}".format(
             self.name, ", ".join(arg_strings), self.return_type
         )
         yield from self.body.generate()
+
+
+class Dataclass(Class):
+    def __init__(self, name, params: list[TypedParam]) -> None:
+        super().__init__(name, None, params)
+
+    def generate(self) -> Iterator[str]:
+        yield "@dataclass"
+        yield from super().generate()
 
 
 class Try(Generable):
