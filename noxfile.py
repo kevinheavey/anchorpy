a54d65678ef9c73eb333e81f9b6a50f0331f7fa3
Kevin Heavey <kevinheavey123@gmail.com>
2022-05-03 22:00:48 +0100

    missing installs in noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,6 @@ from nox_poetry import session  # type: ignore
 
 @session
 def tests(session):  # noqa: D103,WPS442
-    session.run_always("poetry", "install", external=True)
+    session.run_always("poetry", "install", "-E", "cli", external=True)
     session.install(".")
     session.run("pytest", "tests/unit", external=True)
