9af86d8e50568b0f74f4c6a8e7dedc604f6013e5
Kevin Heavey <ID+username@users.noreply.github.com>
2022-10-31 14:58:04 +0000

    remove apischema dep

--- src/anchorpy/idl.py
+++ src/anchorpy/idl.py
@@ -3,25 +3,11 @@ from dataclasses import dataclass, field
 from typing import List, Union, Optional, Dict, Any, Literal, Tuple, TypedDict, Sequence
 from anchorpy_core.idl import IdlTypeDefinition
 
-from apischema import deserialize, alias
-from apischema.metadata import conversion
 from pyheck import snake, upper_camel
 from borsh_construct import CStruct, Vec, U8
 import solana.publickey  # noqa: WPS301
 
 from anchorpy.borsh_extension import BorshPubkey
-
-# We have to define these wrappers because snake and upper_camel
-# don't have annotations
-def _underscore(s: str) -> str:
-    return snake(s)
-
-
-def _camelize(s: str) -> str:
-    return upper_camel(s)
-
-
-snake_case_conversion = conversion(_underscore, _camelize)
 
 def _idl_address(program_id: solana.publickey.PublicKey) -> solana.publickey.PublicKey:
     """Deterministic IDL address as a function of the program id.
