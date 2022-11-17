2cf5b4d60e6cabd9d64047be9f2fb2e54c42aa58
Kevin Heavey <ID+username@users.noreply.github.com>
2022-11-17 23:14:39 +0000

    rough work

---
+++ src/anchorpy/program/namespace/methods.py
@@ -0,0 +1,13 @@
+class MethodsBuilder:
+    def __init__(self, ix_fn: _InstructionFn, )
+
+def _build_methods_item(
+    idl_ix: IdlInstruction,
+    tx_fn: _TransactionFn,
+    idl_errors: Dict[int, str],
+    provider: Provider,
+    program_id: PublicKey,
+) -> MethodsBuilder:
+
+    return MethodsBuilder()
+
