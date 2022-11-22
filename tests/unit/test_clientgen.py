7fd43ea8a1c906f5b9a9b0c2ba25833e073b39d1
Kevin Heavey <ID+username@users.noreply.github.com>
2022-11-22 15:23:39 +0000

    format

--- tests/unit/test_clientgen.py
+++ tests/unit/test_clientgen.py
@@ -9,7 +9,9 @@ def test_gen_accounts() -> None:
     path = Path("tests/idls/composite.json")
     raw = path.read_text()
     idl = Idl.from_json(raw)
-    accs = gen_accounts("CompositeUpdateAccounts", idl.instructions[1].accounts, gen_pdas=True)[0]
+    accs = gen_accounts(
+        "CompositeUpdateAccounts", idl.instructions[1].accounts, gen_pdas=True
+    )[0]
     suite = Suite(accs)
     assert str(suite) == (
         "    class CompositeUpdateAccounts(typing.TypedDict):"
@@ -30,25 +32,25 @@ def test_empty_fields() -> None:
     idl = Idl.from_json(raw)
     struct = gen_struct(idl, "AggregatorLockParams", [])
     assert str(struct) == (
-        'import typing'  # noqa: P103
-        '\nfrom dataclasses import dataclass'
-        '\nfrom construct import Container, Construct'
-        '\nfrom solana.publickey import PublicKey'
-        '\nfrom anchorpy.borsh_extension import BorshPubkey'
-        '\nimport borsh_construct as borsh'
-        '\nclass AggregatorLockParamsJSON(typing.TypedDict):'
-        '\n    pass'
-        '\n@dataclass'
-        '\nclass AggregatorLockParams():'
-        '\n    layout: typing.ClassVar = borsh.CStruct()'
-        '\n    @classmethod'
+        "import typing"  # noqa: P103
+        "\nfrom dataclasses import dataclass"
+        "\nfrom construct import Container, Construct"
+        "\nfrom solana.publickey import PublicKey"
+        "\nfrom anchorpy.borsh_extension import BorshPubkey"
+        "\nimport borsh_construct as borsh"
+        "\nclass AggregatorLockParamsJSON(typing.TypedDict):"
+        "\n    pass"
+        "\n@dataclass"
+        "\nclass AggregatorLockParams():"
+        "\n    layout: typing.ClassVar = borsh.CStruct()"
+        "\n    @classmethod"
         '\n    def from_decoded(cls, obj: Container) -> "AggregatorLockParams":'
-        '\n        return cls()'
-        '\n    def to_encodable(self) -> dict[str, typing.Any]:'
-        '\n        return {}'
-        '\n    def to_json(self) -> AggregatorLockParamsJSON:'
-        '\n        return {}'
-        '\n    @classmethod'
+        "\n        return cls()"
+        "\n    def to_encodable(self) -> dict[str, typing.Any]:"
+        "\n        return {}"
+        "\n    def to_json(self) -> AggregatorLockParamsJSON:"
+        "\n        return {}"
+        "\n    @classmethod"
         '\n    def from_json(cls, obj: AggregatorLockParamsJSON) -> "AggregatorLockParams":'
-        '\n        return cls()'
+        "\n        return cls()"
     )
